423e10bc7907a54d9558059f85fea690a7958b27
Nikita <114725556+NikitaKlyutkin@users.noreply.github.com>
2023-01-13 17:08:01 +0300

    Добвил слои DAL и Domain

---
+++ DiplomaGroomingSalon.DAL/DBContext.cs
@@ -0,0 +1,16 @@
+﻿using DiplomaGroomingSalon.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiplomaGroomingSalon.DAL
+{
+	public class DBContext :DbContext
+	{
+		public DBContext(DbContextOptions opts) : base(opts)
+		{
+			Database.EnsureCreated();
+		}
+
+		public DbSet<User> Users { get; set; }
+		public DbSet<Account> Accounts { get; set; }
+	}
+}
---
+++ GroomingSalon.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiplomaGroomingSalon.Domain.Entities
+{
+	public class User
+	{
+		public Guid Id { get; set; }
+		public string Login { get; set; }
+		public string Password { get; set; }
+		public Account Account { get; set; }
+	}
+}
