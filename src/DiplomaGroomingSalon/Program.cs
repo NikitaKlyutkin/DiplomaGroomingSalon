2abe9f9225b9e10aa646a5ecbb3b3f54df38b0b9
Dmitry Andrienko <d.andrienko@live.com>
2023-03-22 18:56:05 +0300

    add docker compose

--- src/DiplomaGroomingSalon/Program.cs
+++ src/DiplomaGroomingSalon/Program.cs
@@ -1,5 +1,8 @@
+using DiplomaGroomingSalon.DAL;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +16,18 @@ namespace DiplomaGroomingSalon
 	{
 		public static void Main(string[] args)
 		{
-			CreateHostBuilder(args).Build().Run();
+			var builder = CreateHostBuilder(args).Build();
+
+			using (var scope = builder.Services.CreateScope())
+			{
+				using (var db = scope.ServiceProvider.GetService<DBContext>())
+				{
+					db.Database.EnsureCreated();
+					db.Database.Migrate();
+				}
+			}
+				
+			builder.Run();
 		}
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
